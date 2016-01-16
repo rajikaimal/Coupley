b890995eefc1be7c4369dd28826e2b4018bf04cd
rajikaimal <inbox.rajika@gmail.com>
2016-01-16 21:08:26 +0530

    Add meta tag and stylesheets

--- public/init.php
+++ public/init.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="bootstrap.min.css" type="text/css" >
+	<link rel="stylesheet" href="base.css" type="text/css" >
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Coupley</title>
 </head>
-<body>
-	Coupley
+<body style="margin: 0px">
 	<div id="content"></div>
 	<script src="bundle.js"></script>
 </body>
