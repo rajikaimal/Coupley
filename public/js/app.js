62fdaafa629ac33ead34fe4c5b9731268b5a5d03
rajikaimal <inbox.rajika@gmail.com>
2016-01-17 23:09:24 +0530

    Add hashHistory to react-router

--- public/js/app.js
+++ public/js/app.js
@@ -6,18 +6,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //Router components 
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, Link, hashHistory } from 'react-router'
 //starting component
 import Starter from './components/Starter.react';
 import Login from './components/Login.react';
 import Header from './components/base/Header.react';
 import Profile from './components/profile/profile.react';
+import About from './components/profile/About.react';
 import $ from 'jquery';
 
 ReactDOM.render((
-  <Router>
+  <Router history={hashHistory}>
     <Route path="/" component={Header}>
-      <Route path="/profile" component={Profile} />
+      <Route path="profile" component={Profile} >
+      	<Route path="about" component={About} />
+      </Route>
     </Route>
   </Router>
   ),
