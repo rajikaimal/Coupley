8cb450e11719f663d2c40781c16a969c865d8a35
rajikaimal <inbox.rajika@gmail.com>
2016-01-20 01:07:48 +0530

    Route for ActivityFeed in profile

--- public/js/app.js
+++ public/js/app.js
@@ -12,13 +12,14 @@ import Starter from './components/Starter.react';
 import Login from './components/Login.react';
 import Header from './components/base/Header.react';
 import Profile from './components/profile/profile.react';
+import ActivityContainer from './components/profile/ActivityFeed/ActivityFeedContainer.react';
 import About from './components/profile/About.react';
-import $ from 'jquery';
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={Header}>
       <Route path="profile" component={Profile} >
+      	<Route path="activityfeed" component={ActivityContainer} />
       	<Route path="about" component={About} />
       </Route>
     </Route>
