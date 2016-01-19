abd5329a7063749b05b9e75e2962e8e884056e87
rajikaimal <inbox.rajika@gmail.com>
2016-01-20 01:07:20 +0530

    Mock components

---
+++ public/js/components/profile/ProfilePic.react.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const style = {
+	height: 50
+};
+
+const ProfilePic = React.createClass({
+  render: function() {
+    return (
+      <div style={style}>
+        <img src="" />
+      </div>  
+    );    
+  }
+
+});
+
+export default ProfilePic;
--- public/js/components/profile/profile.react.js
+++ public/js/components/profile/profile.react.js
@@ -4,36 +4,59 @@ import CardActions from 'material-ui/lib/card/card-actions';
 import CardHeader from 'material-ui/lib/card/card-header';
 import FlatButton from 'material-ui/lib/flat-button';
 import CardText from 'material-ui/lib/card/card-text';
+import Divider from 'material-ui/lib/divider';
 
-const CardExampleWithoutAvatar = React.createClass({
+import { Link } from 'react-router'
+
+const tabstyle = {
+  marginTop: 30,
+  marginLeft: 50,
+  marginReft: 50
+};
+
+const style = {
+  width: 200,
+  borderRadius: 20
+};
+
+const Profile = React.createClass({
   render: function() {
     return (
-      <Card initiallyExpanded={true}>
-      <CardHeader
-        title="Without Avatar"
-        subtitle="Subtitle"
-        actAsExpander={true}
-        showExpandableButton={true} />
-      <CardText expandable={true}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
-      <CardActions expandable={true}>
-        <FlatButton label="Action1"/>
-        <FlatButton label="Action2"/>
-      </CardActions>
-      <CardText expandable={true}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
-      </Card>
+      <div>
+        <div className="col-lg-9">
+          <div className="panel panel-default">
+            <div className="panel-body">
+              <div>
+                <div className="col-lg-3">
+                  <img src="https://s-media-cache-ak0.pinimg.com/236x/dc/15/f2/dc15f28faef36bc55e64560d000e871c.jpg" style={style} />
+                </div>
+                <div className="col-lg-6">
+                  <h3> Tiffany Hwang </h3>
+                </div>
+                <div className="col-lg-6">
+                  <span> Country etc... </span>
+                </div>
+              </div>
+            </div>
+            <Divider />
+            <div style={tabstyle}>
+              <div className="btn-group btn-group-justified btn-group-info">
+                <Link to={`/profile/activityfeed`} className="btn ">My Activity Feed</Link>
+                <Link to={`/profile/about`} className="btn ">About</Link>
+                <Link to={`/profile/photos`} className="btn ">Photos</Link>
+              </div>
+            </div>
+            {this.props.children}
+          </div>
+        </div>
+
+        <div className="col-lg-3">
+          content
+        </div>
+      </div>
     );    
   }
 
 });
 
-export default CardExampleWithoutAvatar;
+export default Profile;
