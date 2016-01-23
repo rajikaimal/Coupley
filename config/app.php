3b5fbbe547cedfe6068aa00461a3085315877f3e
rajikaimal <inbox.rajika@gmail.com>
2016-01-23 19:57:48 +0530

    Add JWT-AUTH

--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::Class
 
     ],
 
@@ -201,6 +202,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::Class,
+        'JWTFactory'=> Tymon\JWTAuth\Facades\JWTFactory::Class,
 
     ],
 
