e69bba576e392c03bc8420e422425fdbdefe022f
rajikaimal <inbox.rajika@gmail.com>
2016-01-29 11:32:51 +0530

    Add thread routes

---
+++ app/Http/Controllers/SocketController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use LRedis;
+
+class SocketController extends Controller
+{
+    public function __construct()
+	{
+		$this->middleware('guest');
+	}
+	public function sendMessage(Request $request){
+		$redis = LRedis::connection();
+		$redis->publish('message', $request->message);
+	}
+}
