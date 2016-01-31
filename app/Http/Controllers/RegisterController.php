50ef8417d67b32db3b1ce7e4fde7bafd8100ed65
Rajika Imal <inbox.rajika@gmail.com>
2016-01-31 10:32:56 -0500

    Applied fixes from StyleCI

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,41 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use App\User;
 
 class RegisterController extends Controller
 {
-	//checks whether user is already registered
-	public function check(Request $request)
-	{
-		$email = $request->email;
-		$user = User::where('email', $email)->first();
-		if($user == null) {
-			$user = new User;
-	    	$user->firstname = $request->firstname;
-	    	$user->lastname = $request->lastname;
-	    	$user->username = $request->username;
-	    	$user->email = $request->email;
-	    	$user->gender = $request->gender;
-	    	$user->password = \Hash::make($request->password);
-	    	$user->orientation = $request->orientation;
+    //checks whether user is already registered
+    public function check(Request $request)
+    {
+        $email = $request->email;
+        $user = User::where('email', $email)->first();
+        if ($user == null) {
+            $user = new User;
+            $user->firstname = $request->firstname;
+            $user->lastname = $request->lastname;
+            $user->username = $request->username;
+            $user->email = $request->email;
+            $user->gender = $request->gender;
+            $user->password = \Hash::make($request->password);
+            $user->orientation = $request->orientation;
 
-	    	if($user->save()) {
-	    		return response()->json(["status" => 201], 201);
-	    	}
-	    	else {
-	    		return response()->json(["status" => 404], 404);	
-	    	}
-		}
-		else {
-			return response("exists", 200);
-		}
-	}
+            if ($user->save()) {
+                return response()->json(['status' => 201], 201);
+            } else {
+                return response()->json(['status' => 404], 404);
+            }
+        } else {
+            return response('exists', 200);
+        }
+    }
+
     public function register(Request $request)
     {
-    	
     }
 }
--- config/app.php
+++ config/app.php
@@ -155,7 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::Class
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
     ],
 
@@ -202,8 +202,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::Class,
-        'JWTFactory'=> Tymon\JWTAuth\Facades\JWTFactory::Class,
+        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
