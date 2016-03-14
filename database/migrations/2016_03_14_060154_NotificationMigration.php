8754ff85622572c479c198b3fd56145a34e751c8
rajikaimal <inbox.rajika@gmail.com>
2016-03-14 11:47:47 +0530

    typo

---
+++ database/migrations/2016_03_14_060154_NotificationMigration.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NotificationMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id1')->unsigned();
+            $table->foreign('user_id1')->references('id')->on('users');
+            $table->integer('user_id2')->unsigned();
+            $table->foreign('user_id2')->references('id')->on('users');
+            $table->string('content');
+            $table->boolean('read');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
