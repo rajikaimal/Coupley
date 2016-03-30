fd86c349035426bb9a32ccec7cbaa2c4a5bb4cfd
Tharaka <tharakaromesh@gmail.com>
2016-03-30 16:24:49 +0530

    visitors started

---
+++ public/js/components/visits/othersvisits.react.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Card from 'material-ui/lib/card/card';
+import CardActions from 'material-ui/lib/card/card-actions';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardMedia from 'material-ui/lib/card/card-media';
+import CardTitle from 'material-ui/lib/card/card-title';
+import FlatButton from 'material-ui/lib/flat-button';
+import CardText from 'material-ui/lib/card/card-text';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import TextField from 'material-ui/lib/text-field';
+import Divider from 'material-ui/lib/divider';
+import IconButton from 'material-ui/lib/icon-button';
+import Tabs from 'material-ui/lib/tabs/tabs';
+import Tab from 'material-ui/lib/tabs/tab';
+import Dialog from 'material-ui/lib/dialog';
+
+
+
+
+
+const othervisits = React.createClass({
+
+      render:function(){
+             return(
+                <Card>
+                </Card>
+             );
+      }
+
+});
+
+export default othervisits;
