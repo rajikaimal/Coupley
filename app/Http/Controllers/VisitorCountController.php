4a7ed565dd7cba3b37508f49dd664b74907430dc
Rajika Imal <inbox.rajika@gmail.com>
2016-03-30 01:21:19 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/VisitorCountController.php
+++ app/Http/Controllers/VisitorCountController.php
@@ -3,25 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\User;
 
 class VisitorCountController extends Controller
 {
     public function incrementVisitorCount(Request $request)
     {
-    	$username = $request->username;
-    	$visitorUsername = $request->visitorusername;
+        $username = $request->username;
+        $visitorUsername = $request->visitorusername;
 
-    	try {
-    		$user1ID = User::where('username', $username)->get()[0]->id;
-    		$user2ID = User::where('username', $visitorUsername)->get()[0]->id;
+        try {
+            $user1ID = User::where('username', $username)->get()[0]->id;
+            $user2ID = User::where('username', $visitorUsername)->get()[0]->id;
 
-    		//update query using model
-    		//no reponse needed since callback is not used in $.post ... 
-    	} catch (Illuminate\Database\QueryException $e) {
-    		
+            //update query using model
+            //no reponse needed since callback is not used in $.post ...
+        } catch (Illuminate\Database\QueryException $e) {
         }
     }
 }
