d1bcd5c54f796e1e79dbfbba96b9704dbe33bd7e
rajikaimal <inbox.rajika@gmail.com>
2016-03-31 21:34:27 +0530

    query typo fix

--- app/Http/Controllers/LookingForController.php
+++ app/Http/Controllers/LookingForController.php
@@ -20,15 +20,44 @@ class LookingForController extends Controller
         $shortTerm = $request->shortterm;
         $longTerm = $request->longterm;
         $casualSex = $request->casualsex;
+        $location = $request->location;
 
-        //return LookingFor::where('user_id', 11)->get();
+        if($location == "true") {
+            $location = 1;
+        } else {
+            $location = 0;
+        }
 
-        LookingFor::where('user_id', 11)
-            ->update(['location' => 0, 'minage' => 18, 'maxage' => 20,
-                'status' => 0,
-                'shortterm' => 0,
-                'longttterm' => 0,
-                'casualsex' => 0,
+        if($relStatus == "true") {
+            $relStatus = 1;
+        } else {
+            $relStatus = 0;
+        }
+
+        if($shortTerm == "true") {
+            $shortTerm = 1;
+        } else {
+            $shortTerm = 0;
+        }
+
+        if($longTerm == "true") {
+            $longTerm = 1;
+        } else {
+            $longTerm = 0;
+        }
+
+        if($casualSex == "true") {
+            $casualSex = 1;
+        } else {
+            $casualSex = 0;
+        }
+
+        LookingFor::where('user_id', $userID)
+            ->update(['location' => $location, 'minage' => $minAge, 'maxage' => $maxAge,
+                'status' => $relStatus,
+                'shortterm' => $shortTerm,
+                'longtterm' => $longTerm,
+                'casualsex' => $casualSex,
             ]);
 
         return 'done';
