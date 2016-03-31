53e8f100f6fdcc4f7282cb19abd4ac8a7e2c2a03
Rajika Imal <inbox.rajika@gmail.com>
2016-03-31 12:09:47 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/LookingForController.php
+++ app/Http/Controllers/LookingForController.php
@@ -22,31 +22,31 @@ class LookingForController extends Controller
         $casualSex = $request->casualsex;
         $location = $request->location;
 
-        if($location == "true") {
+        if ($location == 'true') {
             $location = 1;
         } else {
             $location = 0;
         }
 
-        if($relStatus == "true") {
+        if ($relStatus == 'true') {
             $relStatus = 1;
         } else {
             $relStatus = 0;
         }
 
-        if($shortTerm == "true") {
+        if ($shortTerm == 'true') {
             $shortTerm = 1;
         } else {
             $shortTerm = 0;
         }
 
-        if($longTerm == "true") {
+        if ($longTerm == 'true') {
             $longTerm = 1;
         } else {
             $longTerm = 0;
         }
 
-        if($casualSex == "true") {
+        if ($casualSex == 'true') {
             $casualSex = 1;
         } else {
             $casualSex = 0;
