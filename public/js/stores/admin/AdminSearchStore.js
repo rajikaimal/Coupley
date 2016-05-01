9d6cbb650d8ec1faffc154bdff77af0e3f955875
isurudilhan <isuru.dilhan@yahoo.com>
2016-05-02 01:15:05 +0530

    Add missing Admin Search controller

---
+++ public/js/stores/admin/AdminSearchStore.js
@@ -0,0 +1,42 @@
+var AppDispatcher = require('../../dispatcher/AppDispatcher');
+var EventEmitter = require('events').EventEmitter;
+var SearchConstants = require('../../constants/SearchConstants');
+var assign = require('object-assign');
+
+var CHANGE_EVENT = 'change';
+
+var searchresults = [];
+
+var SearchStore = assign({}, EventEmitter.prototype, {
+  getresults: function () {
+    return searchresults;
+  },
+
+  saveresults: function (results) {
+    searchresults = results;
+  },
+
+  emitChange: function () {
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener: function (callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener:function (callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+});
+
+AppDispatcher.register(function (payload) {
+  switch (payload.action.actionType) {
+    case (SearchConstants.SEARCH):
+      SearchStore.saveresults(payload.action.search);
+      SearchStore.emitChange();
+      break;
+
+  }
+});
+
+module.exports = SearchStore;
